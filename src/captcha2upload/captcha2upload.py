a55da2dc4b5bb2f5cde8245e7369090ed63f08a1
cdn-db <46291469+cdn-db@users.noreply.github.com>
2019-04-18 23:28:02 +0700

    Update captcha2upload.py
    add report bad captcha solve functions

--- src/captcha2upload/captcha2upload.py
+++ src/captcha2upload/captcha2upload.py
@@ -3,7 +3,7 @@ from requests import post, get
 from time import sleep
 
 
-class CaptchaUpload(object):
+class CaptchaUpload:
     def __init__(self, key, log=None, waittime=None):
         self.settings = {"url_request": "http://2captcha.com/in.php",
                          "url_response": "http://2captcha.com/res.php",
@@ -18,6 +18,30 @@ class CaptchaUpload(object):
             self.waittime = waittime
         else:
             self.waittime = 10
+
+    def report(self,id):
+         """
+        This function return the captcha report
+        :param id: id captcha returned by upload
+        :return: <REPORT> OK | 1 ERROR!
+        """
+        fullurl = "%s?key=%s&action=reportbad&id=%s" % (self.settings['url_response'],
+                                                  self.settings['key'], id)
+        request = get(fullurl)
+        if "OK" in request.text:
+            if self.logenabled:
+                self.log.info("[2CaptchaUpload] Balance: %s" % request.text)
+            return request.text
+        elif request.text == "ERROR_KEY_DOES_NOT_EXIST":
+            if self.logenabled:
+               self.log.error("[2CaptchaUpload] You used the wrong key in "
+                             "the query")
+            return 1
+        elif request.text == "ERROR_WRONG_ID_FORMAT":
+            if self.logenabled:
+                self.log.error("[2CaptchaUpload] Wrong format ID CAPTCHA. "
+                               "ID must contain only numbers")
+            return 1
 
     def getbalance(self):
         """
@@ -87,7 +111,7 @@ class CaptchaUpload(object):
         This function upload read, upload and is the wrapper for solve
             the captcha
         :param pathfile: path of image
-        :return: <captchaword> OK | 1 ERROR!
+        :return: [<id>, <captchaword>] OK | 1 ERROR!
         """
         if exists(pathfile):
             files = {'file': open(pathfile, 'rb')}
@@ -100,56 +124,56 @@ class CaptchaUpload(object):
                 if request.text.split('|')[0] == "OK":
                     if self.logenabled:
                         self.log.info("[2CaptchaUpload] Upload Ok")
-                    return self.getresult(request.text.split('|')[1])
+                    return request.text.split('|')[1],self.getresult(request.text.split('|')[1])
                 elif request.text == "ERROR_WRONG_USER_KEY":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] Wrong 'key' parameter"
                                        " format, it should contain 32 symbols")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_KEY_DOES_NOT_EXIST":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The 'key' doesn't "
                                        "exist")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_ZERO_BALANCE":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] You don't have money "
                                        "on your account")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_NO_SLOT_AVAILABLE":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The current bid is "
                                        "higher than the maximum bid set for "
                                        "your account.")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_ZERO_CAPTCHA_FILESIZE":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] CAPTCHA size is less"
                                        " than 100 bites")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_TOO_BIG_CAPTCHA_FILESIZE":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] CAPTCHA size is more"
                                        " than 100 Kbites")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_WRONG_FILE_EXTENSION":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The CAPTCHA has a "
                                        "wrong extension. Possible extensions "
                                        "are: jpg,jpeg,gif,png")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_IMAGE_TYPE_NOT_SUPPORTED":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The server cannot "
                                        "recognize the CAPTCHA file type.")
-                    return 1
+                    return 1,1
                 elif request.text == "ERROR_IP_NOT_ALLOWED":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The request has sent "
                                        "from the IP that is not on the list of"
                                        " your IPs. Check the list of your IPs "
                                        "in the system.")
-                    return 1
+                    return 1,1
                 elif request.text == "IP_BANNED":
                     if self.logenabled:
                         self.log.error("[2CaptchaUpload] The IP address you're"
@@ -159,9 +183,9 @@ class CaptchaUpload(object):
                                        "authorization keys. To lift the ban, "
                                        "please, contact our support team via "
                                        "email: support@2captcha.com")
-                    return 1
+                    return 1,1
 
         else:
             if self.logenabled:
                 self.log.error("[2CaptchaUpload] File %s not exists" % pathfile)
-            return 1
+            return 1,1
